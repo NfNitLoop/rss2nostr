0fb6bdc062e31d360c85c20bbdf9abefd70b6754
Cody Casterline <cody.casterline@gmail.com>
2022-04-16 13:55:46 -0700

    Use link references in generated markdown.
    This was easy thanks to a new feature in node-html-markdown. Thanks! :D
    https://github.com/crosstype/node-html-markdown/issues/15

--- private/deps.ts
+++ private/deps.ts
@@ -9,16 +9,7 @@ export * as rss from "https://deno.land/x/rss@0.3.6/mod.ts"
 export * as feoblog from "https://deno.land/x/feoblog_client@v0.1.2/mod.ts"
 
 
-// Turndown: https://github.com/domchristie/turndown
-// Note: NO ?dts, because the types reference browser-only types.
-// Aww, blocked on: https://github.com/mixmark-io/turndown/issues/390
-// export { default as TurndownService } from "https://cdn.skypack.dev/turndown@v7.1.1/lib/turndown.umd.js"
-
-// this looks good: 
-// https://github.com/crosstype/node-html-markdown#readme
-// But the types are currently broken by: 
-// https://github.com/denoland/deno/issues/11140
-// export * as h2md from "https://cdn.skypack.dev/node-html-markdown@v0.1.7?dts"
+// Can't import TypeScript types because they reference HTMLElement and Node, which are not built in to Deno.
 // I guess I can use it without typescript types for now:
 export * as nhm from "https://cdn.skypack.dev/node-html-markdown@v1.1.3"
 
--- private/tests/markdown.test.ts
+++ private/tests/markdown.test.ts
@@ -10,6 +10,60 @@ Deno.test("spaces inside styles", () => {
     let input = `Foo<em> <a href="#target">Bar</a> </em>baz`
     let output = htmlToMarkdown(input)
 
+    let expected = unindent(`
+        Foo _[Bar][1]_ baz
+
+        [1]: #target
+    `)
+
     // Make sure this doesn't happen again:
-    assertEquals(output, "Foo _[Bar](#target)_ baz")
+    assertEquals(output, expected)
 })
+
+Deno.test("link refs", () => {
+    let input = unindent(`
+        <p>Here is <a href="https://www.example.com/link1">an example</a>.</p>
+        <p>Here's <a href="https://www.example.com/link2">another</a>.</p>
+        <p>This uses <a href="https://www.example.com/link1">the same link</a> as before.</p>
+    `)
+
+    let output = htmlToMarkdown(input)
+    let expected = unindent(`
+        Here is [an example][1].
+        
+        Here's [another][2].
+        
+        This uses [the same link][1] as before.
+        
+        
+        [1]: https://www.example.com/link1
+        [2]: https://www.example.com/link2
+    `)
+
+    assertEquals(output, expected)
+})
+
+
+// Really? This isn't built into JS or Deno yet?
+function unindent(text: string): string {
+    let lines = text.split("\n")
+    if (lines.length == 1) { return text }
+
+    if (lines[0].trim() != "") {
+        throw new Error(`Expected the first line to be blank but found ${lines[0]}`)
+    }
+    if (lines.at(-1)!.trim() != "") {
+        throw new Error(`Expected the last line to be blank but found ${lines.at(-1)}`)
+    }
+    
+    lines = lines.slice(1, -1)
+
+    let indent = lines[0].match(/^\s*/m)![0]
+
+    lines = lines.map(line => {
+        let trim = line.startsWith(indent) ? indent.length : 0
+        return line.substring(trim)
+    })
+
+    return lines.join("\n")
+}
